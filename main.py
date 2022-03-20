037fce5305f5dbd1e541e5dce16313d2856d72dd
songdlb <songdlb@dcits.com>
2022-03-20 21:53:25 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def numberTest():
+    print("hello")
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    numberTest()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
