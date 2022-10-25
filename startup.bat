a3744b668e8db863e028e933b5d3325e0a020da3
S4TMMY <larry_song@swissre.com>
2022-10-25 22:06:44 +0800

    update startup file

--- startup.bat
+++ startup.bat
@@ -1,5 +1,5 @@
 python -m venv venv
 venv\Scripts\active
 ##venv2\Scripts\deactive
-pip install -r requirements-win.txt
+pip install -r requirements-win.txt -i https://pypi.python.org/simple/
 pip list
